b6ece4178cc0be746a0ee220248b2092f6556fa9
VictorLZA <70566581+VictorLZA@users.noreply.github.com>
2022-04-18 23:26:05 -0400

    Add files via upload

---
+++ server-main-4.18/src/sqlalchemy/utils.py
@@ -0,0 +1,35 @@
+from typing import Optional
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os, json, uuid
+
+from .user import UserDbModel
+from .shared import db_manager
+
+def init_dbmanager(
+    app: Flask, 
+    db_uri: Optional[str] = None, 
+    init_json: Optional[str] = None
+) -> SQLAlchemy:
+    if db_uri is None or len(db_uri) == 0: 
+        db_uri = 'sqlite:///'+ os.path.join(app.root_path,'data.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    db_manager.init_app(app)
+    with app.app_context():
+        db_manager.create_all()
+        if init_json is None: return db_manager
+        for data in json.loads(init_json): 
+            if UserDbModel.find(by_uname=data['username']) is not None: continue
+            new_user = UserDbModel(id=uuid.uuid4().hex, username=data['username'])
+            new_user.set_password(data['password'])
+            db_manager.session.add(new_user)
+        db_manager.session.commit()
+    return db_manager
+
+
+def is_correct(username: str, password: str) -> Optional[UserDbModel]: 
+    user:UserDbModel = UserDbModel.find(by_uname=username)
+    return user is not None and user.validate_password(password)
+    
+
