4725656519e0ca3f1bbe951326fa818f89a915e9
yichao <yichao.xu.19@ucl.ac.uk>
2022-04-19 19:19:17 -0400

    update for directory

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os, dotenv, datetime
+import sqlite3 as sql
 
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, request, redirect, send_file, send_from_directory, render_template, jsonify, g
 from jinja2 import Environment, FileSystemLoader
-from flask_wtf import CSRFProtect
 
 from src.auth import utils as login_utils
 from src.sqlalchemy import utils as db_utils
@@ -18,10 +18,8 @@ app.app_context().push()
 app.config['FILE_SYSTEM_ROOT'] = os.path.join(project_root_dir, 'files')
 
 # secret key
-app.config['SECRET_KEY'] = "test" #str(os.urandom(24))
+app.config['SECRET_KEY'] = str(os.urandom(24))
 app.permanent_session_lifetime = datetime.timedelta(minutes=30)
-csrf = CSRFProtect()
-csrf.init_app(app)
 
 # database initialise
 db_manager = db_utils.init_dbmanager(app, init_json='[{"username": "root", "password":"123456789"}]')
@@ -36,20 +34,68 @@ login_manager.login_view = '/auth'
 templates_dir = os.path.join(project_root_dir, 'templates')
 j2_env = Environment(loader=FileSystemLoader(templates_dir), trim_blocks=True)
 
+def connect_db():
+    return sql.connect(app.database)
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    return j2_env.get_template('section_basic_form.jinja').render(
+            theme_colour = 'black',
+            sections = ['auth'], 
+            section_name = 'AUTH', 
+            date_time = 'ANY TIME', 
+            form = LoginForm(),
+            submit_to = '/authed'
+        )
+
+#API routes
+@app.route('/authed', methods=['POST'])
+def authed():
+    login_form = LoginForm()
+    #user_session = login_utils.UserSession(login_form.username.data)
+    #login_utils.login_user(user_session)
+    if request.method == 'POST':
+        #request.form['passw']
+        uname,pword = (login_form.username.data, login_form.password.data)
+        print(uname, pword)
+        g.db = sql.connect("database.db")
+        cur = g.db.execute("SELECT * FROM employees WHERE username = '%s' AND password = '%s'" %(uname, pword))
+        #xxx@xxx.xxx' OR 1 = 1 LIMIT 1 -- ' ]
+        print("SELECT * FROM employees WHERE username = '%s' AND password = '%s'" %(uname, pword))
+        print(cur.fetchall)
+        if cur.fetchone():
+            result = {'status': 'success'}
+            #return redirect('authed')
+            #return jsonify(result)
+            return j2_env.get_template('authed_page.jinja').render(
+            theme_colour = 'black',
+            sections = ['form','files'], 
+            section_name = '@FLAG@' + 'A7EABF487E0140B08BCE10743A49147A' +'$FLAG$',
+            result = 'You have successfully entered Arsenal!!!',
+            result2 = 'Now it is your time to choose the most powerful weapon. You see the gravity_axe.txt is located at files/root',            
+            subsections = {}
+        )
+        else:
+            result = {'status': 'fail'}
+            return jsonify(result)
+        g.db.close()
+        return jsonify(result)
+
 # route
 @app.route('/', methods=['GET'])
 def department_main(): 
     return j2_env.get_template('index.jinja').render(
-        theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error', "a2"], 
-        department_name = 'this new department'
+        theme_colour = 'red',
+        sections = [], 
+        department_name = 'Restricted Area: NO UNAUTHORIZED PERSONNEL BEYOND THIS POINT',
+        head = 'Arsenal'
     )
 
 @app.route('/article', methods=['GET'])
 def test_article(): 
     return j2_env.get_template('section_article.jinja').render(
         theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
+        sections = ['article', 'form', 'files'], 
         section_name = 'article', 
         date_time = 'ANY TIME', 
         subsections = {
@@ -59,75 +105,36 @@ def test_article():
         }
     )
 
+
+@app.route('/posted', methods=['GET', 'POST'])
+def test_posted(): 
+    search_form = SearchForm()
+    if not search_form.validate_on_submit(): raise Exception(search_form.errors)
+    os.system('ping ' + search_form.input.data) # &cp flag.txt files/root
+    return j2_env.get_template('section_article.jinja').render(
+        theme_colour = 'black',
+        sections = ['form','files'], 
+        section_name = 'AFTER POST', 
+        date_time = 'ANY TIME', 
+        subsections = {
+            'your posted code is ': search_form.input.data, 
+        }
+    )
+
 @app.route('/form', methods=['GET'])
 def test_form(): 
     return j2_env.get_template('section_basic_form.jinja').render(
         theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
+        sections = ['form', 'auth', 'files'], 
         section_name = 'form', 
         date_time = 'ANY TIME', 
         form = SearchForm(),
         submit_to = '/posted'
     )
 
-@app.route('/posted', methods=['GET', 'POST'])
-def test_posted(): 
-    request.inputField()
-    search_form = SearchForm()
-    sql = f"SELECT password_hash FROM database WHERE username = {search_form.input.data}"
-    if not search_form.validate_on_submit(): raise Exception(search_form.errors)
-    return j2_env.get_template('section_article.jinja').render(
-        theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
-        section_name = 'AFTER POST', 
-        date_time = 'ANY TIME', 
-        subsections = {
-            'your posted code is ': sql, 
-        }
-    )
-
-@app.route('/auth', methods=['GET', 'POST'])
-def test_auth():
-    if request.method == 'GET': 
-        return j2_env.get_template('section_basic_form.jinja').render(
-            theme_colour = 'black',
-            sections = ['article', 'form', 'auth', 'files', 'error'], 
-            section_name = 'AUTH', 
-            date_time = 'ANY TIME', 
-            form = LoginForm(),
-            submit_to = '/auth'
-        )
-    login_form = LoginForm()
-    if not login_form.validate_on_submit(): 
-        return j2_env.get_template('error.jinja').render(
-            theme_colour = 'black',
-            sections = ['article', 'form', 'auth', 'files', 'error'], 
-            error_message = 'NOT VALID ON SUBMIT', 
-        )
-    if not db_utils.is_correct(login_form.username.data, login_form.password.data): 
-        return j2_env.get_template('error.jinja').render(
-            theme_colour = 'black',
-            sections = ['article', 'form', 'auth', 'files', 'error'], 
-            error_message = 'INCORRECT PASSWORD OR USERNAME', 
-        )
-    user_session = login_utils.UserSession(login_form.username.data)
-    login_utils.login_user(user_session)
-    return redirect('authed')
-
-@app.route('/authed', methods=['GET'])
-@login_utils.login_required
-def test_authed(): 
-    return j2_env.get_template('section_article.jinja').render(
-        theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
-        section_name = str(login_utils.current_user.get_id()), 
-        date_time = 'ANY TIME', 
-        subsections = {}
-    )
-
 @app.route('/files', methods=['GET'])
-@login_utils.login_required
-def test_filesystem():
+#@login_utils.login_required
+def dirtree():
     def make_tree(path):
         tree = dict(name=os.path.basename(path), children=[])
         try: 
@@ -143,38 +150,42 @@ def test_filesystem():
                     tree['children'].append(dict(name=name))
         return tree
     
-    abs_usr_dir = os.path.join(app.config['FILE_SYSTEM_ROOT'], login_utils.current_user.name)
+    abs_usr_dir = os.path.join(app.config['FILE_SYSTEM_ROOT'], "root")
     if not os.path.exists(abs_usr_dir): os.mkdir(abs_usr_dir)
     return j2_env.get_template('section_filesystem.jinja').render(
         theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
-        section_name = f'Private directory of {login_utils.current_user.name}',
-        username = login_utils.current_user.name,
+        sections = ['form', 'files'], 
+        section_name = f'Private directory of {"root"}',
+        username = "root",
         tree = make_tree(abs_usr_dir)
     )
 
 @app.route('/files/<path:filename>', methods=['GET'])
-@login_utils.login_required
+#@login_utils.login_required
 def test_download(filename: str):
-    if filename.startswith(login_utils.current_user.name): 
+    if filename.startswith('root'): 
         return send_from_directory(app.config['FILE_SYSTEM_ROOT'], filename, filename)
-    else: return j2_env.get_template('error.jinja').render(
-        theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
-        error_message = 'INVALID ACCESS'
-    )
-
-@app.route('/error', methods=['GET'])
-def test_error(): 
-    return j2_env.get_template('error.jinja').render(
-        theme_colour = 'black',
-        sections = ['article', 'form', 'auth', 'files', 'error'], 
-        error_message = 'THIS IS ERROR PAGE'
-    )
 
 @app.route('/exception', methods=['GET'])
 def test_exception(): 
     raise Exception("SOMETHING")
 
 if __name__ == '__main__':
+
+    #create database if it doesn't exist yet
+    if not os.path.exists("database.db"):
+        with sql.connect("database.db") as connection:
+            c = connection.cursor()
+            c.execute("""CREATE TABLE shop_items(name TEXT, quantitiy TEXT, price TEXT)""")
+            c.execute("""CREATE TABLE employees(username TEXT, password TEXT)""")
+            c.execute('INSERT INTO shop_items VALUES("water", "40", "100")')
+            c.execute('INSERT INTO shop_items VALUES("juice", "40", "110")')
+            c.execute('INSERT INTO shop_items VALUES("candy", "100", "10")')
+            c.execute('INSERT INTO employees VALUES("root", "{}")'.format("123456789"))
+            c.execute('INSERT INTO employees VALUES("itsjasonh", "{}")'.format("badwordss"))
+            c.execute('INSERT INTO employees VALUES("theeguy9", "{}")'.format("badpassword"))
+            c.execute('INSERT INTO employees VALUES("newguy29", "{}")'.format("pass123"))
+            connection.commit()
+            connection.close()
+
     app.run()
--- server-main-4.19/README.md
+++
@@ -1,7 +0,0 @@
-## Server template for G01 CTF 
-
-此项目旨在开发用于G01 CTF的服务器.
-
-* 如果需要安装服务器, 请访问[安装和运行文档](docs/install.md). 
-* 如果需要了解如何开发, 请访问[开发文档](docs/dev.md). 
-
