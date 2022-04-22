c1cef015e6e3a01977b328f557f565c51f2f3936
VictorLZA <70566581+VictorLZA@users.noreply.github.com>
2022-04-21 22:19:47 -0400

    Delete server-main directory

--- server-main/src/forms/search_form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class SearchForm(FlaskForm): 
-    input = StringField('Input', validators=[DataRequired(message='⚠️ USERNAME CANNOT BE EMPTY')])
-    submit = SubmitField('Submit')
